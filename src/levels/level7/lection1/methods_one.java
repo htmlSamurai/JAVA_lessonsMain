ccf11a88db0c883c10eb3c5528d64a5f38c04833
htmlSamurai <garik08lsr@gmail.com>
2024-01-14 22:01:25 +0300

    Merge branch 'main' of https://github.com/htmlSamurai/JAVA_lessonsMain

---
+++ src/levels/level7/lection1/methods_one.java
@@ -0,0 +1,50 @@
+package levels.level7.lection1;
+
+import java.util.Arrays;
+
+public class methods_one {
+    public static void main(String[] args) {
+        int[] array = {11, 22, 33, 44, 55, 66, 77, 88, 99};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+        //напишите тут ваш код
+        int dlinna = 0;
+        int first = array[0];
+        int count = 0;
+        int[] copy = new int[array.length];
+        int element_number = 0;
+
+        while ( count != array.length){
+
+
+            for (int i = 1; i < array.length; i++) {
+
+                if (array[i] > array[count]) {
+                    element_number = element_number + 1;
+
+
+                }
+            }
+            copy[element_number] = array[count];
+            count = count + 1;
+
+            element_number = 0;
+        }
+
+        for (int j : copy){
+            System.out.print(j + ", ");
+        }
+        System.out.println();
+        //System.out.println(Arrays.toString(copy));
+    }
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
